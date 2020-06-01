435491e409b94beb543023d4e18e1760f977aea2
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2020-06-01 14:53:43 +0530

    refactor:add layout component DisplayData

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,28 +1,12 @@
 import React from 'react'
-import { Header, SubHeader, Tile, DisplayGrid, Footer } from '../components'
-import clapBoard from '../assets/clapboard.png'
-import { AppState } from '../contexts'
-import { atRule } from 'postcss'
+import { Header, SubHeader, DisplayData, Footer } from '../components'
 
 export const Series = () => {
-  const { state } = AppState()
-  const { status, data } = state
-  console.log(state)
   return (
     <>
       <Header />
       <SubHeader text="Popular Series" />
-
-      {status === 'success' && (
-        <DisplayGrid
-          entries={data.entries.filter(
-            (entry) =>
-              entry.programType === 'series' && entry.releaseYear >= 2010
-          )}
-        />
-      )}
-      {status === 'loading' && <div>Loading...</div>}
-
+      <DisplayData filterByType="series" />
       <Footer />
     </>
   )
