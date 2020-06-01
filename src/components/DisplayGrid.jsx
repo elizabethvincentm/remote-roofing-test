1ac7789c4afc0d6c6738a169f5246d1b7d0a64fe
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2020-06-01 15:04:37 +0530

    feat:add alphanumeric sort for display titles

--- src/components/DisplayGrid.jsx
+++ src/components/DisplayGrid.jsx
@@ -7,7 +7,15 @@ export const DisplayGrid = ({ entries }) => {
     <div className="flex flex-row flex-wrap items-center">
       {entries
         .slice(0, 21)
-        .sort((entry1, entry2) => entry1.title < entry2.title)
+        .sort((entry1, entry2) => {
+          if (entry1.title === entry2.title) {
+            return 0
+          }
+          if (typeof entry1.title === typeof entry2.title) {
+            return entry1.title < entry2.title ? -1 : 1
+          }
+          return typeof entry1.title < typeof entry2.title ? -1 : 1
+        })
         .map((entry) => (
           <Tile
             key={entry.title}
