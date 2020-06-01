996afb626c68f385991f60c99d6bd4be2a5b53d2
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2020-06-01 14:53:43 +0530

    refactor:add layout component DisplayData

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
-export const Tile = ({ url, title, image }) => {
+export const Tile = ({ url, title, image, year }) => {
   return (
-    <div className="flex flex-col justify-between w-32">
-      <img className="w-32 p-2 m-auto" src={image} alt={`${title}`} />
-      <h3 className="tile_title break-words">{title}</h3>
+    <div class="card md:w-1/3 lg:w-1/4 md:px-4 md:mb-8">
+      <article class="h-full cursor-pointer flex flex-col justify-start">
+        <div class="flex-none w-32 md:w-full h-auto">
+          <div class="aspect-ratio-16/9 relative overflow-hidden rounded-sm lg:rounded">
+            <img
+              src={image}
+              alt={`${title}`}
+              class="block absolute w-full h-auto"
+            />
+          </div>
+        </div>
+        <div class="flex-1 mt-auto md:py-6">
+          <h3 class="font-medium text-1xl mb-1 xl:text-xl md:mb-4">{title}</h3>
+          <p class="text-md leading-normal hidden md:block">{year}</p>
+        </div>
+      </article>
     </div>
   )
 }
