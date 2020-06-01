65c32898fb49afa93e0a7e45ba061334cda79640
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2020-06-01 11:30:09 +0530

    feat:fetch data and update series page

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export const Tile = ({ url, title, image }) => {
   return (
-    <Link className="flex flex-col mr-4" to={url}>
-      <div className="bg-secondary relative">
-        <img
-          className="relative bg-secondary inset-0 w-24 p-2 m-auto"
-          src={image}
-          alt="clapboard"
-        />
-      </div>
-
-      <h3 className="tile_title">{title}</h3>
-    </Link>
+    <div className="flex flex-col justify-between w-32">
+      <img className="w-32 p-2 m-auto" src={image} alt={`${title}`} />
+      <h3 className="tile_title break-words">{title}</h3>
+    </div>
   )
 }
--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,5 +1,29 @@
 import React from 'react'
+import { Header, SubHeader, Tile, DisplayGrid, Footer } from '../components'
+import clapBoard from '../assets/clapboard.png'
+import { AppState } from '../contexts'
+import { atRule } from 'postcss'
 
 export const Series = () => {
-  return <div>Series</div>
+  const { state } = AppState()
+  const { status, data } = state
+  console.log(state)
+  return (
+    <>
+      <Header />
+      <SubHeader text="Popular Series" />
+
+      {status === 'success' && (
+        <DisplayGrid
+          entries={data.entries.filter(
+            (entry) =>
+              entry.programType === 'series' && entry.releaseYear >= 2010
+          )}
+        />
+      )}
+      {status === 'loading' && <div>Loading...</div>}
+
+      <Footer />
+    </>
+  )
 }
