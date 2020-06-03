3c3ea7f31df4a683a800eadeaecb4c7e6bf81af2
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2020-06-03 11:44:22 +0530

    feat(deployment): configure github pages

--- src/components/DisplayGrid.jsx
+++ src/components/DisplayGrid.jsx
@@ -4,9 +4,9 @@ import { Tile } from './Tile'
 export const DisplayGrid = ({ entries }) => {
   return (
     <div data-testid="display-grid" className="flex flex-wrap md:-mx-4">
-      {entries.map((entry) => (
+      {entries.map((entry, index) => (
         <Tile
-          key={entry.title}
+          key={`${entry.title}-${index}`}
           title={entry.title}
           year={entry.releaseYear}
           image={entry.images['Poster Art'].url}
