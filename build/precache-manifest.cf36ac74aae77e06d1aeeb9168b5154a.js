ce8f7e6493104ebae5e76accd7e5f6e4a92da8cd
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2020-06-03 11:26:49 +0530

    Updates

---
+++ build/precache-manifest.cf36ac74aae77e06d1aeeb9168b5154a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52248eb6d4ce9b963702786f86b23a4b",
+    "url": "/remote-roofing-test/index.html"
+  },
+  {
+    "revision": "f1e14c347a76a4bb642e",
+    "url": "/remote-roofing-test/static/css/main.c35bc7ba.chunk.css"
+  },
+  {
+    "revision": "7f2900b5a26de85a9589",
+    "url": "/remote-roofing-test/static/js/2.c43071a5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/remote-roofing-test/static/js/2.c43071a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e14c347a76a4bb642e",
+    "url": "/remote-roofing-test/static/js/main.76eca0ba.chunk.js"
+  },
+  {
+    "revision": "e4481ce8dfeb53addf9d",
+    "url": "/remote-roofing-test/static/js/runtime-main.80354f6d.js"
+  },
+  {
+    "revision": "189e324b1b4deb792b85b6df6ee6c8a4",
+    "url": "/remote-roofing-test/static/media/appStore.189e324b.svg"
+  },
+  {
+    "revision": "36ff8623742baa9ab4d3d05cf04b96b9",
+    "url": "/remote-roofing-test/static/media/fb.36ff8623.svg"
+  },
+  {
+    "revision": "b775eda8f6031baaad8721debd4a00aa",
+    "url": "/remote-roofing-test/static/media/googlePlay.b775eda8.svg"
+  },
+  {
+    "revision": "ebd54a475e54d087e76bc10fd4067dcb",
+    "url": "/remote-roofing-test/static/media/insta.ebd54a47.svg"
+  },
+  {
+    "revision": "4b2738dc5e12a504bda62f30dc9b5b15",
+    "url": "/remote-roofing-test/static/media/microsoft.4b2738dc.svg"
+  },
+  {
+    "revision": "c6d938945a670d993463af2e09bfc894",
+    "url": "/remote-roofing-test/static/media/twitter.c6d93894.svg"
+  }
+]);
