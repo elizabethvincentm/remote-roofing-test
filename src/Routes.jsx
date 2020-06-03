e7be45318b2efd86e8efe8e42a7781efd8f94ec6
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2020-06-03 11:55:56 +0530

    feat(routing): switch to hash routing

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import { Home, Series, Movies } from './pages'
 import { Section, Header, Footer } from './components'
 
